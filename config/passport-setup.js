83e6ac1e51ca93e3c47e04c4af2654e5a080550c
kaiyu li <kaiyuli@kaiyude-MacBook-Pro.local>
2019-07-17 06:07:22 +0800

    .

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -8,7 +8,7 @@ passport.use(
       // options for google strategy
       clientID: keys.google.clientID,
       clientSecret: keys.google.clientSecret,
-      callbackURL: 'http://www.kayli.xyz/auth/google/redirect'
+      callbackURL: 'http://www.kayli.xyz:3000/auth/google/redirect'
     },
     (accessToken, refreshToken, profile, done) => {
       // passport callback function
