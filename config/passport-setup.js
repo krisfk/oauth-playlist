858d1b5404cae05c5d3d01456083e9eebb49ee37
kaiyu li <kaiyuli@kaiyude-MacBook-Pro.local>
2019-07-17 06:28:02 +0800

    .

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,8 +1,5 @@
 const passport = require('passport');
-
-//const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-const GoogleStarategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('./keys');
 
