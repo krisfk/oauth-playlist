69d7d9d02c1b07f310c9ddce83f2c8e5bc95d6e8
kaiyu li <kaiyuli@kaiyude-MacBook-Pro.local>
2019-07-17 03:52:19 +0800

    .

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      '803427079907-511ugvldrdqamolcf670rg4cems8hr6b.apps.googleusercontent.com',
+    clientSecret: 'hJuc1GYxXeOwPv5YCZxICb9F'
+  }
+};
