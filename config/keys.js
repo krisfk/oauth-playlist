2fc77bf7fdd8eb0d60991863f11be1c3a64482d1
kaiyu li <kaiyuli@kaiyude-MacBook-Pro.local>
2019-07-17 15:08:45 +0800

    .

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,8 @@
 module.exports = {
   google: {
     clientID:
-      '803427079907-511ugvldrdqamolcf670rg4cems8hr6b.apps.googleusercontent.com',
+      '803427079907-511ugvldrdqamolcf670rg4cfdsfems8hr6b.apps.googleusercontent.com',
     clientSecret: 'hJuc1GYxXeOwPv5YCZxICb9F'
   }
 };
+f;
