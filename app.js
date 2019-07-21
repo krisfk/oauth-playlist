9ad924e1b42cf722e81f9a3d22925432e9d443ec
kaiyu li <kaiyuli@kaiyude-MacBook-Pro.local>
2019-07-21 17:13:54 +0800

    .

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
@@ -9,6 +12,17 @@ const app = express();
 // set view engine
 app.set('view engine', 'ejs');
 
+// set up session cookies
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
     console.log('connected to mongodb');
@@ -16,6 +30,7 @@ mongoose.connect(keys.mongodb.dbURI, () => {
 
 // set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 // create home route
 app.get('/', (req, res) => {
